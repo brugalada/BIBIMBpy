b2a188afa01fbabbfb1e58507a397cc5b69952aa
Pau Ramos <p.ramos@unistra.fr>
2022-11-15 14:52:02 +0900

    Fixed description of runBI and did final run of test notebook

--- bibimbpy/orbits.py
+++ bibimbpy/orbits.py
@@ -24,11 +24,11 @@ def runBI(particle_ini,pot_timedep,df_gen_func,dyn_time_base,number_of_cycles,pa
     Inputs:
     - particle_ini: Initial conditions, Nx6 array of positions and velocites at t=tfin.
     - pot_timedep: AGAMA potential instance use for the backwards integration
-    - pot_base: AGAMA potential instance. The potential at t<=0, before the perturbation. Must account for the mass that will later be put in the perturbation (unless adding mass to the system is the desired outcome).
     - df_gen_func: Arbritrary function that, given a vector Nx6 of positions and velocities in an intertial frame, returns the value of the Distribution Function for each of the particles in pot_base at t<=0, before the perturbation.
     - dyn_time_base: Dynamical timescale. It is meant to be used as the period of the pattern speed, i.e. 2pi/pattern_speed [in units of s*kpc/km]
     - number_of_cycles: Number of dynamical timescales to integrate.
     - pattern_speed: pattern speed of the rotating frame, positive for a prograre rotation [in units of km/s/kpc]
+    -_Npoints: Number of snapshots. In case you want to see the whole orbit, you can increase the number of sample taken along the whole orbit.
 
     Outputs:
     - df_eval: value of the Distribution Function for each particle in particle_ini. 
